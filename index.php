f0758558abe4507604e902b6f6be7098f71a4dff
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-20 20:43:50 -0300

    Menú agregado con include

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
-<a href="mesa">Mesa de Entradas</a><br>
-<a href="secretaria">Secretaría</a><br>
-<a href="presidencia">Presidencia</a><br>
-<a href="labor">Labor Parlamentria</a><br>
-<a href="prosecretaria">Prosecretaría</a><br>
-<a href="comisiones">Secretaría de Comisiones</a>
+<?php include('conexion.php'); ?>
+<?php include('menu.php'); ?>
