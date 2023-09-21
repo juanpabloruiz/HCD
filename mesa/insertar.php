7111cc23459e2c5b6a6f5a71b0820fe78d822f39
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-20 21:56:18 -0300

    Quitando código de prueba

--- mesa/insertar.php
+++ mesa/insertar.php
@@ -8,10 +8,5 @@ $autor = $_POST['autor'];
 $arreglo = implode(' - ', $autor);
 $fecha = $_POST['fecha'];
 mysqli_query($conexion, "INSERT INTO mesa (expte, extracto, letra, paginas, autor, fecha) VALUES ('$expte', '$extracto', '$letra', '$paginas', '$arreglo', '$fecha')");
-//mysqli_query($conexion, "INSERT INTO secretaria (expte) VALUES ('$expte')");
-//mysqli_query($conexion, "INSERT INTO presidencia (expte) VALUES ('$expte')");
-//mysqli_query($conexion, "INSERT INTO labor (expte) VALUES ('$expte')");
-//mysqli_query($conexion, "INSERT INTO prosecretaria (expte) VALUES ('$expte')");
-//mysqli_query($conexion, "INSERT INTO comisiones (expte) VALUES ('$expte')");
 header('Location:./');
 ?>
