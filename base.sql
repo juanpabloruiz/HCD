f1276d9a0980e6ad12167075905175bdd130474b
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-21 21:04:48 -0300

    Base de datos con usuarios

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-09-2023 a las 23:09:19
+-- Tiempo de generación: 22-09-2023 a las 00:04:16
 -- Versión del servidor: 11.1.2-MariaDB
 -- Versión de PHP: 8.2.10
 
@@ -174,7 +174,7 @@ CREATE TABLE `usuarios` (
   `correo` varchar(255) DEFAULT NULL,
   `clave` varchar(255) DEFAULT NULL,
   `nombre` varchar(255) DEFAULT NULL,
-  `tipo` varchar(255) DEFAULT NULL,
+  `categoria` varchar(255) DEFAULT NULL,
   `creado` datetime DEFAULT NULL,
   `editado` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
@@ -183,10 +183,43 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `correo`, `clave`, `nombre`, `tipo`, `creado`, `editado`) VALUES
+INSERT INTO `usuarios` (`id`, `correo`, `clave`, `nombre`, `categoria`, `creado`, `editado`) VALUES
 (1, 'pabloruiz1980@gmail.com', 'soledad', 'Ruiz, Juan Pablo', 'Administrador', '2023-09-20 15:00:21', '2023-09-20 17:17:29'),
-(2, 'mesa@hcd.ar', 'mesa', 'Romero, Javier Alejandro', 'Concejal', '2023-09-20 15:00:59', '2023-09-20 20:56:27'),
-(3, 'secre@hcd.ar', 'secretaria', 'González, Alejandra Victoria', 'Concejal', '2023-09-20 15:01:55', '2023-09-20 20:56:22');
+(2, 'mesa@hcd.ar', 'mesa', 'Romero, Javier Alejandro', 'Editor', '2023-09-20 15:00:59', '2023-09-20 20:56:27'),
+(4, NULL, NULL, 'Concejal Vallejos, Alfredo', 'Autor', '2023-09-21 20:28:59', NULL),
+(5, NULL, NULL, 'Concejal Duarte Ojeda, Cecilia', 'Autor', '2023-09-21 20:31:20', NULL),
+(6, NULL, NULL, 'Concejal Achitte, Eduardo', 'Autor', '2023-09-21 20:31:53', NULL),
+(7, NULL, NULL, 'Concejal Ibañes, Esteban', 'Autor', '2023-09-21 20:32:19', NULL),
+(8, NULL, NULL, 'Concejal Nieves, Fabián', 'Autor', '2023-09-21 20:32:52', NULL),
+(9, NULL, NULL, 'Concejal Ojeda, Florencia', 'Autor', '2023-09-21 20:33:18', NULL),
+(10, NULL, NULL, 'Concejal Gauna, Gabriela', 'Autor', '2023-09-21 20:33:49', NULL),
+(11, NULL, NULL, 'Concejal Torres, Héctor', 'Autor', '2023-09-21 20:34:14', NULL),
+(12, NULL, NULL, 'Concejal Miranda Gallino, Julian', 'Autor', '2023-09-21 20:34:44', NULL),
+(13, NULL, NULL, 'Concejal Almirón, Lisandro', 'Autor', '2023-09-21 20:35:08', NULL),
+(14, NULL, NULL, 'Concejal Acevedo Caffa, Lorena', 'Autor', '2023-09-21 20:35:32', NULL),
+(15, NULL, NULL, 'Concejal Duarte, Magdalena', 'Autor', '2023-09-21 20:36:05', NULL),
+(16, NULL, NULL, 'Concejal Mecca, Melisa', 'Autor', '2023-09-21 20:36:25', NULL),
+(17, NULL, NULL, 'Concejal Franco Laprovitta, Mercedes', 'Autor', '2023-09-21 20:37:01', NULL),
+(18, NULL, NULL, 'Concejal Mestres, Mercedes', 'Autor', '2023-09-21 20:37:33', NULL),
+(19, NULL, NULL, 'Concejal Mosquera, Manuel', 'Autor', '2023-09-21 20:37:56', NULL),
+(20, NULL, NULL, 'Concejal Lovera, Nelsón', 'Autor', '2023-09-21 20:38:28', NULL),
+(21, NULL, NULL, 'Concejal Vallejos, Sofía', 'Autor', '2023-09-21 20:38:52', NULL),
+(22, NULL, NULL, 'Concejal Barrios, Yamandú', 'Autor', '2023-09-21 20:39:28', NULL),
+(23, NULL, NULL, 'Señores Concejales', 'Autor', '2023-09-21 20:39:56', NULL),
+(24, NULL, NULL, 'Bloque Unión Cívica Radical', 'Autor', '2023-09-21 20:40:38', NULL),
+(25, NULL, NULL, 'Bloque Ciudadanos a Gobernar', 'Autor', '2023-09-21 20:41:04', NULL),
+(26, NULL, NULL, 'Bloque Partido Nuevo', 'Autor', '2023-09-21 20:41:38', NULL),
+(27, NULL, NULL, 'Bloque Coalición Cívica Ari', 'Autor', '2023-09-21 20:42:08', NULL),
+(28, NULL, NULL, 'Bloque Partido Liberal', 'Autor', '2023-09-21 20:43:24', NULL),
+(29, NULL, NULL, 'Bloque Unión Celeste y Blanca', 'Autor', '2023-09-21 20:44:00', NULL),
+(30, NULL, NULL, 'Bloque Partido Justicialista', 'Autor', '2023-09-21 20:44:54', NULL),
+(31, NULL, NULL, 'Bloque JP Descamisados. Partido Justicialista', 'Autor', '2023-09-21 20:45:45', NULL),
+(32, NULL, NULL, 'Bloque Partido Popular', 'Autor', '2023-09-21 20:46:12', NULL),
+(33, NULL, NULL, 'Bloque PRO', 'Autor', '2023-09-21 20:46:49', NULL),
+(34, NULL, NULL, 'Bloque Nuevo Encuentro', 'Autor', '2023-09-21 20:47:18', NULL),
+(35, NULL, NULL, 'Bloque Ciudadanos Comprometidos', 'Autor', '2023-09-21 20:47:41', NULL),
+(36, NULL, NULL, 'Bloque Encuentro Liberal', 'Autor', '2023-09-21 20:48:01', NULL),
+(37, NULL, NULL, 'Bloque Unión Popular', 'Autor', '2023-09-21 20:48:39', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -278,7 +311,7 @@ ALTER TABLE `secretaria`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(133) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(133) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
