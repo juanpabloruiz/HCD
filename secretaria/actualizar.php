4190af3790b093bcd4f58e96ea8f127bff5f83c7
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-22 20:32:20 -0300

    Campos agregados

--- secretaria/actualizar.php
+++ secretaria/actualizar.php
@@ -4,25 +4,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include('../conexion.php');
 $id = $_POST['id'];
-$archivo = $_POST['archivo'];
-$expte = $_POST['expte'];
-$adjunto = $_POST['adjunto'];
-$extracto = $_POST['extracto'];
-$letra = $_POST['letra'];
-$paginas = $_POST['paginas'];
-$autores = implode(' - ', $_POST['autor']);
-$estado = $_POST['estado'];
-$tipo = $_POST['tipo'];
-mysqli_query($conexion, "UPDATE mesa SET
-expte = '$expte',
-adjunto = '$adjunto',
-extracto = '$extracto',
-letra = '$letra',
-paginas = '$paginas',
-autor = '$autores',
-estado = '$estado',
-tipo = '$tipo',
-editado = NOW()
+$informe = $_POST['informe'];
+$firma = $_FILES['firma']['name'];
+move_uploaded_file($_FILES['firma']['tmp_name'], '../firmas/' . $firma);
+$lugar_fecha = $_POST['lugar_fecha'];
+mysqli_query($conexion, "UPDATE secretaria SET
+informe = '$informe',
+firma = '$firma',
+lugar_fecha = '$lugar_fecha',
 WHERE id = '$id'");
 header('Location:./');
 ?>
