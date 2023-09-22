aee5196c46c0826c3a6d97bdadd99de12f05d71f
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-22 09:57:41 -0300

    Organizando espacios de código

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php include('cabecera.php'); ?>
 <?php include('menu.php'); ?>
 
-<main class="container my-3">
+    <main class="container my-3">
 
-<form method="post" action="ingreso">
-    
-    <div class="mb-3">
-        <input type="email" name="correo" class="form-control" placeholder="Correo electrónico">
-    </div>
+        <form method="post" action="ingreso">
+            
+            <div class="mb-3">
+                <input type="email" name="correo" class="form-control" placeholder="Correo electrónico">
+            </div>
 
-    <div class="mb-3">
-        <input type="password" name="clave" class="form-control" placeholder="Contraseña">
-    </div>
+            <div class="mb-3">
+                <input type="password" name="clave" class="form-control" placeholder="Contraseña">
+            </div>
 
-    <div class="mb-3">
-        <input type="submit" value="Ingresar" class="btn btn-primary">
-    </div>
-        
-</form>
+            <div class="mb-3">
+                <input type="submit" value="Ingresar" class="btn btn-primary">
+            </div>
+                
+        </form>
 
+    </main>
 
-</main>
 <?php include('pie.php'); ?>
