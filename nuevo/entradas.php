83175da713c936f8b127d4acd4a226e536acb7d2
juanpabloruiz <pabloruiz1980@gmail.com>
2023-09-25 19:05:51 -0300

    Arreglando los enlaces HCD

--- nuevo/entradas.php
+++ nuevo/entradas.php
@@ -23,7 +23,7 @@ mysqli_set_charset($conexion, 'utf8');
 <body>
     <nav data-bs-theme="dark" class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">HCD</a>
+            <a class="navbar-brand" href="./">HCD</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
